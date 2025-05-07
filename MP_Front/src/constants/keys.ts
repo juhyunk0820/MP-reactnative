504bf1c2f40953d693004130fbe90ac418b51445
강주현 <juhyunk0820@naver.com>
2025-05-07 23:29:19 +0900

    src/constants :상수 관련 Recovery

---
+++ MP_Front/src/constants/keys.ts
@@ -0,0 +1,23 @@
+const queryKeys = {
+  AUTH: 'auth',
+  GET_ACCESS_TOKEN: 'getAccessToken',
+  GET_PROFILE: 'getProfile',
+  MARKER: 'marker',
+  GET_MARKERS: 'getMarkers',
+  POST: 'post',
+  GET_POST: 'getPost',
+  GET_POSTS: 'getPosts',
+  FAVORITE: 'favorite',
+  GET_FAVORITE_POSTS: 'getFavoritePosts',
+  GET_CALENDAR_POSTS: 'getCalendarPosts',
+} as const;
+
+const storageKeys = {
+  REFRESH_TOKEN: 'refreshToken',
+  THEME_MODE: 'themeMode',
+  THEME_SYSTEM: 'themeSystem',
+  SHOW_LEGEND: 'showLegend',
+  MARKER_FILTER: 'markerFilter',
+} as const;
+
+export {queryKeys, storageKeys};
