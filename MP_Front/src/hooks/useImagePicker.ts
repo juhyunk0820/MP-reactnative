67c526b1df66da6123a7945a1a7195a5ea786322
강주현 <juhyunk0820@naver.com>
2025-05-07 23:31:17 +0900

    api 및 기능 관련 hook 복구

---
+++ MP_Front/src/hooks/useImagePicker.ts
@@ -0,0 +1,87 @@
+import {getFormDataImages} from '@/utils';
+import ImagePicker from 'react-native-image-crop-picker';
+import useMutateImages from './queries/useMutateImages';
+import {useState} from 'react';
+import {ImageUri} from '@/types';
+import {Alert} from 'react-native';
+import Toast from 'react-native-toast-message';
+
+interface UseImagePickerProps {
+  initialImages: ImageUri[];
+  mode?: 'single' | 'multiple';
+  onSettled?: () => void;
+}
+
+function useImagePicker({
+  initialImages = [],
+  mode = 'multiple',
+  onSettled,
+}: UseImagePickerProps) {
+  const uploadImages = useMutateImages();
+  const [imageUris, setImageUris] = useState(initialImages);
+  const addImageUris = (uris: string[]) => {
+    if (imageUris.length + uris.length > 5) {
+      Alert.alert('이미지 개수 초과', '추가 가능한 이미지는 최대 5개 입니다.');
+    }
+    setImageUris(prev => [...prev, ...uris.map(uri => ({uri}))]);
+  };
+
+  const replaceImageUri = (uris: string[]) => {
+    if (uris.length > 1) {
+      Alert.alert('이미지 개수 초과', '이미지는 최대 1개만 변경 가능합니다.');
+      return;
+    }
+
+    setImageUris([...uris.map(uri => ({uri}))]);
+  };
+
+  const deleteImageUri = (uri: string) => {
+    const newImageUris = imageUris.filter(image => image.uri !== uri);
+    setImageUris(newImageUris);
+  };
+
+  const changeImageUrisOrder = (fromIndex: number, toIndex: number) => {
+    const copyImageUris = [...imageUris];
+    const [removedImage] = copyImageUris.splice(fromIndex, 1);
+    copyImageUris.splice(toIndex, 0, removedImage);
+    setImageUris(copyImageUris);
+  };
+
+  const handleChange = () => {
+    ImagePicker.openPicker({
+      mediaType: 'photo',
+      multiple: true,
+      includeBase64: true,
+      maxFiles: mode === 'multiple' ? 5 : 1,
+      cropperChooseText: '완료',
+      cropperCancelText: '취소',
+    })
+      .then(images => {
+        const formData = getFormDataImages(images);
+
+        uploadImages.mutate(formData, {
+          onSuccess: data =>
+            mode === 'multiple' ? addImageUris(data) : replaceImageUri(data),
+          onSettled: () => onSettled && onSettled(),
+        });
+      })
+      .catch(error => {
+        if (error.code !== 'E_PICKER_CANCELLED') {
+          Toast.show({
+            type: 'error',
+            text1: '갤러리를 열 수 없습니다.',
+            text2: '권한 허용을 다시 한번 확인해 주세요.',
+            position: 'top',
+          });
+        }
+      });
+  };
+
+  return {
+    imageUris,
+    handleChange,
+    delete: deleteImageUri,
+    changeOrder: changeImageUrisOrder,
+  };
+}
+export default useImagePicker;
