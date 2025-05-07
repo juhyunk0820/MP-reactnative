00012a96d8784acfe59b356db2d9d364cd8f9af9
강주현 <juhyunk0820@naver.com>
2025-05-08 00:38:43 +0900

    splash screen Objective-C Bridging-Header file

---
+++ MP_Front/ios/MP-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MP-Bridging-Header.h
+//  MP_Front
+//
+//  Created by 강주현 on 5/7/25.
+//
+
+#ifndef MP_Bridging_Header_h
+#define MP_Bridging_Header_h
+
+
+#endif /* MP_Bridging_Header_h */
+#import "RNSplashScreen.h"
