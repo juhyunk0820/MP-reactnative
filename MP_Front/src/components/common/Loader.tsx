bd03be4270bfba2b9ded0fff94afe6a95b3e5027
강주현 <juhyunk0820@naver.com>
2025-05-13 21:55:27 +0900

    Loading Componenet: Suspense를 이용한 사용

---
+++ MP_Front/src/components/common/Loader.tsx
@@ -0,0 +1,39 @@
+import React, {PropsWithChildren} from 'react';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  ActivityIndicatorProps,
+} from 'react-native';
+import {colors} from '@/constants';
+
+function Loader({
+  children,
+  size = 'small',
+  color = colors.light.GRAY_500,
+  ...props
+}: PropsWithChildren<ActivityIndicatorProps>) {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator
+        size={size}
+        color={color}
+        style={styles.indicator}
+        {...props}
+      />
+    </View>
+  );
+}
+
+export default Loader;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  indicator: {
+    marginBottom: 20,
+  },
+});
